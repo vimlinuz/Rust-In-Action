0c159a0c538ca7c2ba95be7b7b58d52fff6490c8
santoshxshrestha <santoshxshrestha@gmail.com>
2026-01-22 21:11:27 +0545

    some objects are to be converted with type safety

---
+++ serilization/src/main.rs
@@ -0,0 +1,39 @@
+#![allow(unused)]
+use serde::{Deserialize, Serialize};
+use serde_json;
+use serde_json::json;
+
+#[derive(Serialize, Debug, Deserialize)]
+struct Data {
+    x: i32,
+    y: i32,
+}
+impl Data {
+    fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
+}
+
+fn main() {
+    let origin = Data::new(0, 0);
+    let serialized = serde_json::to_string(&origin).unwrap();
+    println!("origin: {:?}", origin);
+    println!("origin: {}", serialized);
+
+    let json_data = json!({
+        "name": "santosh",
+        "age": 21,
+        "sanity cleck": 100
+    });
+    println!("the json data is: {json_data:#?} ");
+
+    let rando_point = json!({
+        "x": 12,
+        "y": 12,
+    });
+
+    // we have to conver the data into string as teh rando_point is a type of Value which this
+    // method doesn't takes
+    let rando_with_type: Data = serde_json::from_str(&rando_point.to_string()).unwrap();
+    println!("The rando data is: {:#?}", rando_with_type);
+}
