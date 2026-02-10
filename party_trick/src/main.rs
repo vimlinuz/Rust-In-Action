09ea2a6bc963772d9c68e3a881646c047bbcd372
santoshxshrestha <santoshxshrestha@gmail.com>
2026-02-11 00:39:40 +0545

    hasher

---
+++ party_trick/src/main.rs
@@ -0,0 +1,41 @@
+use std::fmt;
+
+struct Message {
+    content: String,
+    hasher: u32,
+}
+
+impl Message {
+    fn new(content: String, hasher: u32) -> Self {
+        Self { content, hasher }
+    }
+
+    fn crypt(self) -> Self {
+        Self {
+            content: self
+                .content
+                .chars()
+                .map(|c| char::from_u32(c as u32 ^ self.hasher).expect("unicode conversion error"))
+                .collect::<String>(),
+            hasher: self.hasher,
+        }
+    }
+}
+
+impl fmt::Display for Message {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.content)
+    }
+}
+
+fn main() {
+    let message = Message::new(String::from("nigger"), 1);
+
+    println!("The original message is: {}", message);
+
+    let encrypted_message = message.crypt();
+    println!("The encrypted message is: {}", encrypted_message);
+
+    let plane_message = encrypted_message.crypt();
+    println!("The plane message again is: {}", plane_message);
+}
