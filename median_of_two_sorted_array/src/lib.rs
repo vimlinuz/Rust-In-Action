3da50449787044af3503bbd7bdab00ca715c0a67
vimlinuz <vimlinuz@gmail.com>
2026-04-24 21:43:50 +0545

    rm var

--- median_of_two_sorted_array/src/lib.rs
+++ median_of_two_sorted_array/src/lib.rs
@@ -2,46 +2,46 @@ pub struct Solution;
 impl Solution {
     pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
         let mut nums3 = Vec::new();
-        let nums1_length = nums1.len();
-        let nums2_length = nums2.len();
-        let nums3_length = nums1_length + nums2_length;
+
+        let nums1_len = nums1.len();
+        let nums2_len = nums2.len();
 
         let mut nums1_index = 0;
         let mut nums2_index = 0;
 
-        while nums1_index < nums1.len() {
-            if nums2_index < nums2_length {
+        loop {
+            if nums1_index < nums1_len && nums2_index < nums2_len {
                 if nums1[nums1_index] < nums2[nums2_index] {
                     nums3.push(nums1[nums1_index]);
                     nums1_index += 1;
-                } else if nums1[nums1_index] == nums2[nums2_index] {
-                    nums3.push(nums1[nums1_index]);
+                } else if nums1[nums1_index] > nums2[nums2_index] {
                     nums3.push(nums2[nums2_index]);
-                    nums1_index += 1;
                     nums2_index += 1;
                 } else {
                     nums3.push(nums2[nums2_index]);
+                    nums3.push(nums1[nums1_index]);
+                    nums1_index += 1;
                     nums2_index += 1;
                 }
-            } else {
+            } else if nums1_index < nums1_len {
                 nums3.push(nums1[nums1_index]);
                 nums1_index += 1;
+            } else if nums2_index < nums2_len {
+                nums3.push(nums2[nums2_index]);
+                nums2_index += 1;
+            } else {
+                break;
             }
         }
-        while nums2_index < nums2_length {
-            nums3.push(nums2[nums2_index]);
-            nums2_index += 1;
-        }
 
-        if nums3_length % 2 == 0 {
-            let index = nums3_length / 2;
-            let first_number = nums3[index];
-            let second_number = nums3[index - 1];
-            let median = (first_number + second_number) as f64 / 2 as f64;
-            return median;
+        let nums3_len = nums1_len + nums2_len;
+
+        if nums3_len % 2 == 0 {
+            let index = nums3_len / 2;
+
+            (nums3[index] + nums3[index - 1]) as f64 / 2 as f64
         } else {
-            let index = (nums3_length - 1) / 2;
-            return nums3[index] as f64;
+            nums3[(nums3_len - 1) / 2] as f64
         }
     }
 }
