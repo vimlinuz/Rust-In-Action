059d2d50d080e497ecd9c154887ffde306bc22f1
vimlinuz <vimlinuz@gmail.com>
2026-04-24 21:27:55 +0545

    add: done median of sorted array

--- median_of_two_sorted_array/src/lib.rs
+++ median_of_two_sorted_array/src/lib.rs
@@ -1,19 +1,46 @@
 pub struct Solution;
 impl Solution {
     pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
-        let mut nums3 = nums1;
-        nums3.extend(nums2);
-        nums3.sort();
-        let length = nums3.len();
+        let mut nums3 = Vec::new();
+        let nums1_length = nums1.len();
+        let nums2_length = nums2.len();
+        let nums3_length = nums1_length + nums2_length;
 
-        if length % 2 == 0 {
-            let index = length / 2;
+        let mut nums1_index = 0;
+        let mut nums2_index = 0;
+
+        while nums1_index < nums1.len() {
+            if nums2_index < nums2_length {
+                if nums1[nums1_index] < nums2[nums2_index] {
+                    nums3.push(nums1[nums1_index]);
+                    nums1_index += 1;
+                } else if nums1[nums1_index] == nums2[nums2_index] {
+                    nums3.push(nums1[nums1_index]);
+                    nums3.push(nums2[nums2_index]);
+                    nums1_index += 1;
+                    nums2_index += 1;
+                } else {
+                    nums3.push(nums2[nums2_index]);
+                    nums2_index += 1;
+                }
+            } else {
+                nums3.push(nums1[nums1_index]);
+                nums1_index += 1;
+            }
+        }
+        while nums2_index < nums2_length {
+            nums3.push(nums2[nums2_index]);
+            nums2_index += 1;
+        }
+
+        if nums3_length % 2 == 0 {
+            let index = nums3_length / 2;
             let first_number = nums3[index];
             let second_number = nums3[index - 1];
             let median = (first_number + second_number) as f64 / 2 as f64;
             return median;
         } else {
-            let index = (length - 1) / 2;
+            let index = (nums3_length - 1) / 2;
             return nums3[index] as f64;
         }
     }
---
+++ median_of_two_sorted_array/src/main.c
@@ -0,0 +1,16 @@
+
+#include <stdlib.h>
+double findMedianSortedArrays(int *nums1, int nums1Size, int *nums2,
+                              int nums2Size) {
+  int lenghtOfNums1 = nums1Size / sizeof(int);
+  int lenghtOfNums2 = nums2Size / sizeof(int);
+  int lenghtOfNums3 = lenghtOfNums1 + lenghtOfNums2;
+  int *nums3 = (int *)malloc(sizeof(int) * lenghtOfNums3);
+
+  int greater = 0;
+  int index_of_nums3 = 0;
+
+  for (int i = 0; i < lenghtOfNums1; i++) {
+        if (nums1[])
+  }
+}
